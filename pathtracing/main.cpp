9136fe41919348540736d0489a22d9a781c4fe73
Joschua Loth <joschua.loth@gmx.de>
2020-08-03 08:03:11 +0200

    some incemental improvements 2

--- pathtracing/main.cpp
+++ pathtracing/main.cpp
@@ -211,7 +211,7 @@ int main(int argc, char** argv)
   //helloVk.loadModel(nvh::findFile("media/scenes/CornellBox-Glossy-Floor.obj", defaultSearchPaths));
   //helloVk.loadModel(nvh::findFile("media/scenes/plane.obj", defaultSearchPaths));
 
-  helloVk.loadModel(nvh::findFile("media/scenes/ladies/gumi_alone.obj", defaultSearchPaths));
+  helloVk.loadModel(nvh::findFile("media/scenes/ladies/gumi_alone_highp.obj", defaultSearchPaths));
   helloVk.loadModel(nvh::findFile("media/scenes/ladies/walls.obj", defaultSearchPaths));
   nvmath::vec4f plc = {10, 10, 10, 1};
   nvmath::vec4f b   = {0, 0, 0, 1};
